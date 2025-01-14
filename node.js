ac765e52cd4915e799cee9789e27ae19cfc1cbcc
miaover61 <sigmiko145@gmail.com>
2025-01-14 19:25:13 +0900

    Create node.js

---
+++ node.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const WhitePage: React.FC = () => {
+  return (
+    <div className="min-h-screen bg-white flex items-center justify-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-gray-800">White Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default WhitePage;
